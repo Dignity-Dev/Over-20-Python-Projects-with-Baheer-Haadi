25217c062cfefc59097119de2cce8704330140d2
devfaoed <bashirhaadiy@gmail.com>
2023-06-13 22:25:03 +0100

    Find and replace app using python

--- find_and_replace.py
+++ find_and_replace.py
@@ -2,9 +2,11 @@ def replace_word():
     
     content = "Welcome to python programming, in this lesson lesson we we will talk on find and replace"
     word_to_find = input("Find What: ")
-   
-    replace_with = input( "Enter replacement: ")
-    print(content.replace(word_to_find, replace_with))
+    if word_to_find not in content:
+        print("word not found!")
+    else:
+        replace_with = input( "Enter replacement: ")
+        print(content.replace(word_to_find, replace_with))
    
 
 
